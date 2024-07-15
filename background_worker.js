7c8ba009e0969a487a9fe96a0c1bac6a9d7ed355
UnnoticedMan <francisco@laura.lat>
2024-07-15 16:02:10 +0200

    Bloqueador de anuncios funcional para youtube 2024 v2

--- background_worker.js
+++
@@ -1,7 +0,0 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    return { cancel: true };
-  },
-  { urls: ["*://*.googlevideo.com/*ad*", "*://*.google.com/*ad*", "*://*.doubleclick.net/*"] },
-  ["blocking"]
-);
